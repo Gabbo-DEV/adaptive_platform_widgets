e582939005e37af3edcec713597b8b4ef76339d9
Gabriele <gabrieletroccoli90@gmail.com>
2022-09-13 17:42:54 +0200

    first year

--- lib/widgets/adaptive_alert.dart
+++ lib/widgets/adaptive_alert.dart
@@ -50,90 +50,36 @@ class AdaptiveAlert {
     );
   }
 
-  static Future<bool?> error({
-    required BuildContext context,
-    required Widget title,
-    required String content,
-  }) {
+  static Future<void> error(BuildContext context,
+      {Widget title = const Text('Error'), Widget content = const Text('')}) {
     if (!Platform.isIOS) {
       return showDialog<bool>(
         context: context,
         builder: (context) => AlertDialog(
           title: title,
-          content: _errorContent(context, content),
           actions: <Widget>[
             TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: const Text('Ok'),
+              onPressed: () {},
+              child: const Text('OK'),
             ),
           ],
         ),
       );
     }
-
     return showCupertinoDialog<bool>(
       context: context,
       builder: (context) => CupertinoAlertDialog(
         title: title,
-        content: _errorContent(context, content),
         actions: <Widget>[
           CupertinoDialogAction(
-            onPressed: () => Navigator.of(context).pop(true),
-            child: const Text('Ok', style: TextStyle(color: Colors.white)),
+            onPressed: () {},
+            child: const Text(
+              'Ok',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
-    );
-  }
-
-  static Widget _errorContent(BuildContext context, String content) {
-    return Stack(
-      clipBehavior: Clip.none,
-      alignment: Alignment.topCenter,
-      children: [
-        Container(
-          margin: EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * .04,
-          ),
-          padding: const EdgeInsets.only(
-            top: 32,
-            left: 16,
-            right: 16,
-            bottom: 12,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(24),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(content),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(true),
-                    child: const Text('Ok'),
-                  ),
-                ],
-              ),
-              Positioned(
-                  top: -28,
-                  child: CircleAvatar(
-                    minRadius: 16,
-                    maxRadius: 28,
-                    backgroundColor: Theme.of(context).colorScheme.error,
-                    child: const Icon(
-                      Icons.assistant_photo,
-                      size: 28,
-                      color: Colors.white,
-                    ),
-                  ))
-            ],
-          ),
-        )
-      ],
     );
   }
 }
